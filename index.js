e47fe86144cf90d5f62d99f27b19aae7291b761c
TZ <atian25@qq.com>
2017-03-08 08:30:52 +0800

    feat: pass proc to parent

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const spawn = require('child_process').spawn;
  * Run shell script in child process
  * Support OSX, Linux and Windows
  * @param {String} script - full script string, like `git clone https://github.com/node-modules/runscript.git`
- * @param {Object} [options] - spawn options
- *   @see https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
- * @return {Object} stdio object, will contains stdio.stdout and stdio.stderr buffer.
+ * @param {Object} [options] - spawn options, @see https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
+ * @param {Function} [options.onStart] - callback with `proc` when start
+ * @return {Promise} stdio object, will contains stdio.stdout and stdio.stderr buffer.
  */
 module.exports = function runScript(script, options) {
   return new Promise((resolve, reject) => {
@@ -30,9 +30,16 @@ module.exports = function runScript(script, options) {
       options.windowsVerbatimArguments = true;
     }
 
+    const onStart = options.onStart;
+    options.onStart = undefined;
+
     const proc = spawn(sh, [ shFlag, script ], options);
     const stdout = [];
     const stderr = [];
+
+    if (typeof onStart === 'function') {
+      onStart(proc);
+    }
     if (proc.stdout) {
       proc.stdout.on('data', buf => {
         stdout.push(buf);
